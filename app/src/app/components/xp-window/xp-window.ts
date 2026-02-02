f422ef1ad47ae9c868f03564de2b5d08b145a421
Steve <stevenegeer@gmail.com>
2026-02-02 11:08:04 -0800

    Update Angular to v21 (#1)

---
+++ app/src/app/components/xp-window/xp-window.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aim-xp-window',
+  imports: [],
+  templateUrl: './xp-window.html',
+  styleUrl: './xp-window.css',
+})
+export class XpWindow {
+
+}
