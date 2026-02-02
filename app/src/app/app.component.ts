00e66a825aabff60ac2ad259d02b17c1be823b7c
Steve G <stevenegeer@gmail.com>
2026-02-02 08:54:59 -0800

    Port client basics
    This get's all the files needed for the client to build properly.
    Components, services, etc will be brought in piecemeal with tests later.

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from "@angular/router";
 import { invoke } from "@tauri-apps/api/core";
 
 @Component({
-  selector: "app-root",
+  selector: "aim-root",
   imports: [RouterOutlet],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
