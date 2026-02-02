97cce580a2087bddab9798159dfa1ad75bcb4f50
Steve <stevenegeer@gmail.com>
2026-02-02 14:52:13 -0800

    Port chat window and form (#11)
    * Bring in the ChatWindow route component
    * Add ChatForm component
    
    NOTE: Because this is a route component and the current code doesn't facilitate opening new windows, test will be written once the window can actually be tested.

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from "@angular/router";
 import { SignIn } from "./routes/sign-in/sign-in";
+import { ChatWindow } from "./routes/chat/chat";
 
 export const routes: Routes = [
   {
     path: "",
     pathMatch: "full",
     component: SignIn,
+  },
+  {
+    path: "chat",
+    component: ChatWindow,
   },
 ];
