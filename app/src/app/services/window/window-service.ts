df814ae69bab94e62eac11c069d001a67b62b1fa
Steve G <stevenegeer@gmail.com>
2026-02-04 20:29:16 -0800

    Add process exit to the sign-in close button

--- app/src/app/services/window/window-service.ts
+++ app/src/app/services/window/window-service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { getCurrentWindow } from "@tauri-apps/api/window";
+import { exit } from "@tauri-apps/plugin-process";
 
 /**
  * The tests wherever the window service is being injected need to be updated
@@ -17,7 +18,12 @@ export class WindowService {
     return await getCurrentWindow().show();
   }
 
-  async close() {
+  async close(doExit: boolean = false) {
+    // TODO: Confirm with dialog, but this has potentially been buggy with the window drag API
+    if (doExit) {
+      await exit().catch(console.warn);
+    }
+
     return await getCurrentWindow().close();
   }
 
