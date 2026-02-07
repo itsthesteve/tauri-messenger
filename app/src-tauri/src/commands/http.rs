11c4638b69ee7cc2dfee116a2720b5c2bd01620e
Steve G <stevenegeer@gmail.com>
2026-02-06 20:57:34 -0800

    Add tauri http plugin with json feature

---
+++ app/src-tauri/src/commands/http.rs
@@ -0,0 +1,27 @@
+use std::time::Duration;
+
+use tauri::http::{header, HeaderValue};
+use tauri_plugin_http::reqwest::{Client, ClientBuilder};
+
+pub fn get_http_client() -> Result<Client, std::string::String> {
+    let mut headers = header::HeaderMap::new();
+    headers.insert("X-Aim", HeaderValue::from_static("0.1.0"));
+    headers.insert(
+        header::CONTENT_TYPE,
+        HeaderValue::from_static("application/json"),
+    );
+
+    let http_client = ClientBuilder::new()
+        .default_headers(headers)
+        .timeout(Duration::from_secs(5))
+        .build()
+        .map_err(|err| {
+            if err.is_timeout() {
+                return String::from("server timed out; is it running?");
+            }
+
+            format!("unknown error creating http client: {}", err)
+        });
+
+    http_client
+}
--- app/src-tauri/src/commands/screen_names.rs
+++ app/src-tauri/src/commands/screen_names.rs
@@ -1,7 +1,24 @@
+use crate::commands::get_http_client;
+use serde_json::{json, Value};
+
 #[tauri::command]
 pub async fn create_screen_name(
     screen_name: &str,
     password: &str,
 ) -> Result<(), std::string::String> {
+    let client = get_http_client().unwrap();
+    let payload = json!({
+        "screenName": screen_name,
+        "password": password
+    });
+
+    let request = client
+        .post("http://localhost:8080/api/v1/createScreenName")
+        .json(&payload)
+        .send()
+        .await;
+
+    // YAH: Finish
+
     Ok(())
 }
